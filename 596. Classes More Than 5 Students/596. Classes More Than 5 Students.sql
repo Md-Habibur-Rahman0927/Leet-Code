4932fce352eb632d161351ad9b2feadb8553dda1
Md-Habibur-Rahman0927 <mdhabibur927543@gmail.com>
2023-12-23 17:01:31 +0600

    596. Classes More Than 5 Students

--- 596. Classes More Than 5 Students/596. Classes More Than 5 Students.sql
+++ 596. Classes More Than 5 Students/596. Classes More Than 5 Students.sql
@@ -3,3 +3,8 @@ from (select `class`, count(distinct `student`) as `num`
      from `courses`
      group by `class`) as `tmp_table` 
 where `num` >= 5;
+
+# Write your MySQL query statement below
+SELECT class FROM Courses
+GROUP BY class
+HAVING COUNT(student) >= 5;
