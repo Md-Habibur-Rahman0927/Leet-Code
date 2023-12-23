0b9ac1a1daaab4a9af8aacf03b6283b0f7a8ce45
Md-Habibur-Rahman0927 <mdhabibur927543@gmail.com>
2023-12-24 00:10:08 +0600

    1907. Count Salary Categories

--- 1907. Count Salary Categories/1907. Count Salary Categories.sql
+++ 1907. Count Salary Categories/1907. Count Salary Categories.sql
@@ -18,3 +18,17 @@ select t.category, ifnull(a.cnt, 0) as accounts_count
         group by 1
     ) as a
     on t.category = a.category;
+
+
+# Write your MySQL query statement below
+select 'Low Salary' as category
+, count(*) as accounts_count
+from accounts where income<20000
+union all
+select 'High Salary' as category
+, count(*) as accounts_count
+from accounts where income>50000
+union all
+select 'Average Salary' as category
+, count(*) as accounts_count
+from accounts where income between 20000 and 50000
